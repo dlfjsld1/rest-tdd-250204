089ce096964d24be823fe6e7dbf1a63a18e686c3
dlfjsld1 <ltk900216@gmail.com>
2025-02-04 14:20:52 +0900

    join-r

--- src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
+++ src/test/java/com/example/rest_tdd/RestTddApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.rest_tdd;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class RestTddApplicationTests {
+@ActiveProfiles("test")
+//서버를 띄우지 않아도 스프링이 내부적으로 네트워크에서 실행한 것처럼 작동
+@AutoConfigureMockMvc
+//DB 작업을 해도 테스트 환경에서는 원래대로 돌아가게 함
+@Transactional
+class AuthApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
 
 	@Test
-	void contextLoads() {
+	@DisplayName("회원가입")
+	void join() throws Exception {
+		ResultActions resultActions = mvc
+				.perform(
+						post("/api/v1/member/join")
+				)
+				.andDo(print());
+		resultActions
+				.andExpect(status().isCreated());
 	}
 
 }
