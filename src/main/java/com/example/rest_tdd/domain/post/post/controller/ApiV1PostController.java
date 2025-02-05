b5c96c7adca7472834ed5e26200c7167f546ae73
dlfjsld1 <ltk900216@gmail.com>
2025-02-05 14:33:18 +0900

    post modify no apiKey

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
@@ -7,6 +7,8 @@ import com.example.rest_tdd.domain.post.post.service.PostService;
 import com.example.rest_tdd.global.Rq;
 import com.example.rest_tdd.global.dto.RsData;
 import com.example.rest_tdd.global.exception.ServiceException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,11 +38,17 @@ public class ApiV1PostController {
     }
 
 
-    record WriteReqBody(String title, String content) {};
+    record WriteReqBody(
+            @NotBlank
+            String title,
+            @NotBlank
+            String content
+    ) {}
 
     @PostMapping
     public RsData<PostDto> write(
             @RequestBody
+            @Valid
             WriteReqBody reqBody
     ) {
         Member actor = rq.getAuthenticatedActor();
@@ -49,6 +57,38 @@ public class ApiV1PostController {
         return new RsData<>(
                 "201-1",
                 "%d번 글 작성이 완료되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
+    }
+
+
+    record ModifyReqBody(
+            @NotBlank
+            String title,
+            @NotBlank
+            String content
+    ) {}
+
+    @PutMapping("{id}")
+    public RsData<PostDto> modify(
+            @PathVariable
+            long id,
+            @RequestBody
+            ModifyReqBody reqBody
+    ) {
+
+        Member actor = rq.getAuthenticatedActor();
+
+        Post post = postService.getItem(id).orElseThrow(() ->
+                new ServiceException("404-1", "존재하지 않는 글입니다."
+                ));
+
+        post.canModify(actor);
+        postService.modify(post, reqBody.title(), reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id),
                 new PostDto(post)
         );
     }
