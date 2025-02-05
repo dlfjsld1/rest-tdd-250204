539359ae6bc02c7d573be042e44e7a458856730b
dlfjsld1 <ltk900216@gmail.com>
2025-02-05 09:49:44 +0900

    loginRequest

--- src/test/java/com/example/rest_tdd/ApiV1MemberControllerTest.java
+++ src/test/java/com/example/rest_tdd/ApiV1MemberControllerTest.java
@@ -103,14 +103,8 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.msg").value("이미 사용중인 아이디입니다."));
     }
 
-    @Test
-    @DisplayName("로그인 - 성공")
-    void login1() throws Exception {
-
-        String username = "user1";
-        String password = "user11234";
-        // 요청
-        ResultActions resultActions = mvc
+    private ResultActions loginRequest(String username, String password) throws Exception {
+        return mvc
                 .perform(
                         post("/api/v1/members/login")
                                 .content("""
@@ -126,8 +120,18 @@ public class ApiV1MemberControllerTest {
                                 )
                 )
                 .andDo(print());
+    }
 
+    @Test
+    @DisplayName("로그인 - 성공")
+    void login1() throws Exception {
+
+        String username = "user1";
+        String password = "user11234";
+        // 요청
+        ResultActions resultActions = loginRequest(username, password);
         Member member = memberService.findByUsername(username).get();
+
         // 응답. (요청 처리 결과)
         resultActions
                 .andExpect(status().isOk())
@@ -152,23 +156,7 @@ public class ApiV1MemberControllerTest {
         String username = "user1";
         String password = "1234";
         // 요청
-        ResultActions resultActions = mvc
-                .perform(
-                        post("/api/v1/members/login")
-                                .content("""
-                                        {
-                                            "username": "%s",
-                                            "password": "%s"
-                                        }
-                                        """
-                                        .formatted(username, password)
-                                        .stripIndent())
-                                .contentType(
-                                        new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
-                                )
-                )
-                .andDo(print());
-
+        ResultActions resultActions = loginRequest(username, password);
 
         // 응답. (요청 처리 결과)
         resultActions
