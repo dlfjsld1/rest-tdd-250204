8954abd67a24c1b9919efb5c85b4450c96d4c433
dlfjsld1 <ltk900216@gmail.com>
2025-02-06 11:26:58 +0900

    search r

--- src/main/java/com/example/rest_tdd/domain/post/post/service/PostService.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/service/PostService.java
@@ -71,7 +71,7 @@ public class PostService {
     public Page<Post> getListedItems(int page, int pageSize) {
 
         //페이징 처리
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
+        PageRequest pageRequest = PageRequest.of(page - 1, pageSize);
 
         return postRepository.findByListed(true, pageRequest);
     }
